7f628654ff59ec295b9119f5307ae7f8088a1584
Bijay Timilsina <bjtimi.007@gmail.com>
2019-05-21 15:43:36 +0200

    test logs

--- data/resolvers.js
+++ data/resolvers.js
@@ -11,7 +11,6 @@ const resolvers = {
   Query: {
     getMockChat: () => {
       const date = new Date();
-      console.log('DATE ORGINAL: ', date);
       const mockChat = {
         id: '_' + Date.now(),
         created: date.toLocaleString(),
@@ -27,6 +26,7 @@ const resolvers = {
   Mutation: {
     createMessage(parent, {sender, message}, { pubsub }) {
       const date = new Date();
+      console.log('DATE ORGINAL: ', date);
       const newChatAdded = {
         id: '_' + Date.now(),
         created: date.toLocaleString(),
