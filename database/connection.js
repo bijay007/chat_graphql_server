85fb64e4a8179f6b160edbb3747b462e4543ade7
Bijay Timilsina <bjtimi.007@gmail.com>
2019-05-22 13:27:13 +0200

    Add remote DB_URI

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
+const local_Uri = 'mongodb://localhost:27017/local_chat';
 
 const connectToDB = () => {
-  mongoose.connect('mongodb://localhost:27017/local_chat', {useNewUrlParser: true});
+  mongoose.connect(process.env.DB_URI || local_Uri, {useNewUrlParser: true});
   mongoose.connection
     .once('open', () => { console.log('Connected to mongodb.')})
     .once('close', () => { console.log('Connection closed.')})
